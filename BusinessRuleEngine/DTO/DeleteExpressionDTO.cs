881240ccbbc625c05324bb1f381f838bb1de4c49
928james <46875020+928james@users.noreply.github.com>
2023-02-13 22:07:01 -0800

    James Eddins DTOs
    DTOs added to edit and delete expressions and rules

---
+++ BusinessRuleEngine/DTO/DeleteExpressionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessRuleEngine.DTO
+{
+    /*
+     * This class is meant to be used on the AddExpression controller to retreive a new expression to add 
+     * TODO: Still needs to change the actions and values datatype
+     */
+
+    public class DeleteExpressionDTO
+    {
+        public string expressionID { get; init; }
+    }
+}
---
+++ BusinessRuleEngine/DTO/EditExpressionDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace BusinessRuleEngine.DTO
+
+/*
+ * This class is meant to be used on the AddRule controller to retreive a new rule to add 
+ * TODO: Still needs to change the actions and values datatype
+ */
+
+{
+    public record EditExpressionDTO
+    {
+        public string ExpressionID { get; init; }
+
+        // TODO: Possibly change from init to set because ExpressionID should already exist in rule table
+
+        public string LeftOperandType { get; init; }
+
+        public string LeftOperandValue { get; init; }
+
+        public string RightOperandType { get; init; }
+
+        public string RightOperandValue { get; init; }
+
+        public string Operator { get; init; }
+    }
+}
